229f7b47632a25c61d7598436f0dc3aacdb06bcb
Divyam19 <divyamgupta19@gmail.com>
2024-02-27 16:23:13 +0530

    basic homepage before carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { ThemeProvider } from '@emotion/react'
 import { darktheme } from './theme/DatkTheme'
+import HomePage from './pages/HomePage'
 
 
 function App(){
   return (
     <>
       <ThemeProvider theme={darktheme}>
-        <h1 className='text-3xl font-bold underline'>Hello Wold!!</h1>
+        <HomePage/>
       </ThemeProvider>
     </>
   )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import "./HomePage.css"
 const HomePage = () => {
   return (
     <div>
-        <section>
-
+        <section className='-z-50 banner relative flex flex-col justify-center items-center'>
+            <div className='w-[50vw] z-10 tet-center'>
+                <p className='text-2xl lg:text-7xl font-bold z-10 py-5 text-center'>
+                    Tango
+                </p>
+                <p className='z-10 text-gray-300 text-xl lg:text-4xl text-center'>Taste the convenience: food delivery at a moments notice</p>
+            </div>
+            <div className='cover top-0 left-0 right-0'></div>
+            <div className='fadeout'>1212</div>
         </section>
     </div>
   )
